7dc58650326789a7c074332ccfb500509aa70f1e
serena2910 <82798472+serena2910@users.noreply.github.com>
2021-04-19 09:44:58 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,24 @@
+		<?php
+		//https://api.telegram.org/bot118569545:AAFlvM1DcIhbs63TlnF_bBFmEZ4Vni2inyQ/setwebhook?url=https://siabot.herokuapp.com/execute.php
+		// PARAMETRI DA MODIFICARE
+		//inserite vostro indirizzo su heroku 
+		//https://username.herokuapp.com/execute.php
+		$WEBHOOK_URL = 'https://serenaaa.herokuapp.com/';
+		//inserite il vostro token che vi ha fornito l'utente telegram @botfather
+		$BOT_TOKEN = '1700124117:AAE70rwoTKQesZQvI8BzEF7ZRk55884bYTk';
+
+		// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+		$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+		$method = 'setWebhook';
+		$parameters = array('url' => $WEBHOOK_URL);
+		$url = $API_URL . $method. '?' . http_build_query($parameters);
+		
+		$handle = curl_init($url);
+
+		
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+		$result = curl_exec($handle);
+		print_r($result);
+		?>
